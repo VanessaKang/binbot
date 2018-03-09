d18c0d2277476d7bd24164fe0a317c6d113498d6
vanessakang <kangv2@mcmaster.ca>
2018-03-09 19:32:51 +0000

    Uploaded code for obtaining RSSI on the main code

--- binbotmaintest/csvRead.cpp
+++ binbotmaintest/csvRead.cpp
@@ -8,13 +8,8 @@
 
 int main() {
     int count=0;
-
-    std::vector<int> ei_state1;
-    std::vector<char*> beaconVal;
-    std::vector<int> ei_userCommand1;
-    std::vector<int> ei_error1;
-
     std::ifstream inFile;
+	printf("Working666\n");
     while(1){
 
     inFile.open("beaconvalues.txt");
@@ -29,27 +24,17 @@ int main() {
     char thisVal[MAXSIZE];
     
     while (inFile.getline(thisVal,MAXSIZE,',')) {
-        //std::stoi(thisVal);
-        //std::cout << thisVal;
-        //printf("\n");
-        //ei_state1.push_back(std::stoi(thisVal));
-	//end of vector
 	values.push_back(thisVal);
     }
     
     inFile.close();
 
-    //std::cout << ei_state[0];
 
     printf("print each vector \n");
-    //int i= ei_state.front();
     int i = 0;
     for(i; i<values.size();i++){
         std::cout << values[i] << "\n";
     }
-    /*for(i; i<beaconVal.size();i++){
-        std::cout << beaconVal[i] << "\n";
-    }*/
     
     printf("vector size: %i \n", values.size());
     usleep(2000000);
